70ce17ddf97c1a6401b96264ab140f32d7952b58
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-02-02 21:06:22 +0600

    feat: update input field text color to black in CartDrawer component

--- app/components/CartDrawer.tsx
+++ app/components/CartDrawer.tsx
@@ -203,7 +203,7 @@ export const CartDrawer: React.FC = () => {
                       }}
                       onKeyPress={(e) => e.key === 'Enter' && handleApplyPromo()}
                       placeholder="Enter code"
-                      className="flex-1 px-3 py-2 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      className="flex-1 px-3 py-2 border text-black border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                     <button
                       onClick={handleApplyPromo}
